72a08a6ea431715cbd86ac3a3eae2e1d67232559
Bouke Regnerus <bouke@regner.us>
2018-03-20 13:32:38 +0100

    Updated classify

--- classify.py
+++ classify.py
@@ -1,20 +1,35 @@
 import os
 import numpy as np
 from scipy.io import wavfile
+import pandas as pd
 
 from pydub import AudioSegment
 from audio.processor import WavProcessor, format_predictions
 
-sig = AudioSegment.from_file('train1.wav', format="wav")
+df = pd.read_csv('labels.csv')
+df = df.set_index('index')
 
-start = int(0 * 1000)
-end = int(100 * 1000)
-segment = sig[start:end]
-samples = np.array(segment.get_array_of_samples())
-new_sig = samples.astype(np.float32)
+# print(df)
 
-with WavProcessor() as proc:
-    predictions = proc.get_predictions(16000, new_sig)
+sig = AudioSegment.from_file('kitchen.wav', format="wav")
+
+_start = 0
+_end = 6
+
+for x in range(0, 100):
+    start = int(_start * 1000)
+    end = int(_end * 1000)
+    segment = sig[start:end]
+    samples = np.array(segment.get_array_of_samples())
+    new_sig = samples.astype(np.float32)
+
+    with WavProcessor() as proc:
+        predictions = proc.get_predictions(16000, new_sig)
+
+        for p in predictions:
+            print(str(_start/60), ":", str(_end/60), " - ", df.loc[p[0]].values[1], p[1])
     
-    for p in predictions:
-        print(p)
+    _start += 6
+    _end += 6
+    
+
